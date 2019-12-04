a46398cf9a458128b6c0b3109628a4ad6668002d
pedelriomarron <pedelriomarron@gmail.com>
2019-12-04 16:57:35 +0100

    Creacion del Servidor

---
+++ server/server.js
@@ -0,0 +1,37 @@
+require("./config/config")
+const express = require('express')
+const app = express()
+//CONFIG
+
+
+
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.get('/usuario', (req, res) => {
+    res.json("get Usuario")
+})
+app.post('/usuario', (req, res) => {
+    let body = req.body;
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        })
+    }
+    res.json({ body })
+})
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id
+    res.json({ id })
+})
+app.delete('/usuario', (req, res) => {
+    res.json("delete  Usuario")
+})
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Escuchando peteciones por el puerto ${process.env.PORT}`)
+})
